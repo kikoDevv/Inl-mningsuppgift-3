a9c9ebc24cd4fab676ea492475cece5628b0addf
Nasrolla <Nasrolla.hassani@gmail.com>
2024-09-20 22:03:53 +0200

    kommentar är klart.

--- onClick.js
+++ onClick.js
@@ -1,4 +1,5 @@
 function byt(){
+    /*när denna funktion blir roppad så körs dem här koden för att justera h1.*/
     document.querySelector("#text").innerHTML = "Goodbye, world!";
     document.querySelector("#text").style.color="red";
     document.querySelector("#knappen").style.backgroundColor="white";
