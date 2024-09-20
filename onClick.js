ea55ae16703bdf88467fdd9beb5399cebff6effc
Nasrolla <Nasrolla.hassani@gmail.com>
2024-09-20 21:48:14 +0200

    final

---
+++ onClick.js
@@ -0,0 +1,5 @@
+function byt(){
+    document.querySelector("#text").innerHTML = "Goodbye, world!";
+    document.querySelector("#text").style.color="red";
+    document.querySelector("#knappen").style.backgroundColor="white";
+}
